a9caad564228943bf4ccf3bbdfaa84a98158b83f
Ben Richards <benjamindrichards@gmail.com>
2018-09-25 12:23:44 +1200

    Update timer blocks to use Time Helper world clock.

--- src/kiwifroot-generators.js
+++ src/kiwifroot-generators.js
@@ -32,6 +32,101 @@ Blockly.JavaScript[ "kiwi_instance_exists" ] = function( block ) {
 };
 
 
+
+
+/*
+Overrides for blocks already in the core.
+*/
+
+
+
+
+
+// Override clock type.
+Blockly.Kiwifroot['kiwi_event_time'] = function(block) {
+
+  var funcName = defineFunctionFromBranch('onTick', block, 'Executed after time period of time.');
+
+  var tick = Blockly.Kiwifroot.valueToCode(block, 'MILLISECOND', Blockly.Kiwifroot.ORDER_ASSIGNMENT) || 16;
+
+
+  var constructorCode = errorCheck( this.workspace, ('!' + tick), '`Interval Time Event` block is missing a numeric value.' ) +  
+    'this.' + funcName + '_ = this.state.clocks.world.setInterval( function() { \n' +
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT + 'if( this.owner && this.owner.exists ) {\n' +
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT + 'this.' + funcName + '();\n' + 
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT + '}\n' + 
+    Blockly.Kiwifroot.INDENT + '}, ' + tick + ', this);';
+
+  var destructorCode = 'if( this.' + funcName + '_ ) this.' + funcName + '_.stop();\n';
+  destructorCode += 'this.state.clocks.world.removeTimer( this.' + funcName + '_ );';
+
+  Blockly.Kiwifroot.provideAddition(Blockly.Kiwifroot.CONSTRUCTOR,constructorCode);
+  Blockly.Kiwifroot.provideAddition(Blockly.Kiwifroot.DESTRUCTOR, destructorCode);
+
+  return null;
+};
+
+// Override clock type.
+Blockly.Kiwifroot['kiwi_event_time_single'] = function(block) {
+
+  var funcName = Blockly.Kiwifroot.variableDB_.getDistinctName('onSingleTick', Blockly.Procedures.NAME_TYPE);
+  var branch = Blockly.Kiwifroot.statementToCode(block, 'STACK');
+
+  var tick = Blockly.Kiwifroot.valueToCode(block, 'MILLISECOND', Blockly.Kiwifroot.ORDER_ASSIGNMENT) || 16; 
+
+  var code = errorCheck( this.workspace, ('!' + tick), '`Single Time Control` block is missing a numeric value.' ) + 
+    'this.' + funcName + '_ = this.state.clocks.world.setTimeout( function() { \n' + 
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT + 'if( this.owner && this.owner.exists ) {\n' +
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT + branch +  
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT + '}\n' +
+    Blockly.Kiwifroot.INDENT + '}, ' + tick + ', this);\n';
+  
+  var destructorCode = 'if( this.' + funcName + '_ ) this.' + funcName + '_.stop();\n'; 
+  destructorCode += 'this.state.clocks.world.removeTimer( this.' + funcName + '_ );';
+  Blockly.Kiwifroot.provideAddition(Blockly.Kiwifroot.DESTRUCTOR, destructorCode);
+
+  return code;
+};
+
+// Override clock type.
+Blockly.Kiwifroot[ "kiwi_scratch_events_wait" ] = function( block ) {
+
+  // Functionality copied from `kiwi_event_time_single`,
+  // except this takes seconds instead of millis.
+
+  var funcName = Blockly.Kiwifroot.variableDB_.getDistinctName(
+      "wait", Blockly.Procedures.NAME_TYPE );
+  var branch = Blockly.Kiwifroot.statementToCode( block, "STACK" );
+  var tick = Blockly.Kiwifroot.valueToCode(
+    block, "SECS", Blockly.Kiwifroot.ORDER_ASSIGNMENT ) || 1;
+  var code = "";
+  var destructorCode = "";
+
+  code += errorCheck(
+      this.workspace,
+      ( "!" + tick ),
+      "`Wait` block is missing a numeric value." ) +
+    "this." + funcName + "_ = " +
+    "this.state.clocks.world.setTimeout( function() {\n" +
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT +
+    "if ( this.owner && this.owner.exists ) {\n" +
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT +
+    branch +
+    Blockly.Kiwifroot.INDENT + Blockly.Kiwifroot.INDENT +
+    "}\n" +
+    Blockly.Kiwifroot.INDENT + "}, " + tick + " * 1000, this );\n";
+
+  destructorCode +=
+    "if ( this." + funcName + "_ ) this." + funcName + "_.stop();\n" +
+    "this.state.clocks.world.removeTimer( this." + funcName + "_ );";
+
+  Blockly.Kiwifroot.provideAddition(
+    Blockly.Kiwifroot.DESTRUCTOR, destructorCode );
+
+  return code;
+};
+
+
 // Override buggy definition.
 // The original version didn't check whether the `getGameSoundByID` call
 // returned structured data, and could crash the whole game on compile
