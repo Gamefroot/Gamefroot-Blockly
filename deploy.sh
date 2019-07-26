1a6008efde297cdffc1337a12bf59cb10d4f2154
Ben Richards <benjamindrichards@gmail.com>
2019-07-26 17:27:48 +1200

    Fix deploy script and add toolbox XML to deployment.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+gsutil cp blockly_toolbox.xml gs://static.gamefroot.com/blockly/2.2.0/
 gsutil cp -r src gs://static.gamefroot.com/blockly/2.2.0/
-gsutil -m setmeta -h "Cache-Control:public, max-age=60" gs://static.gamefroot.com/blockly/2.1.0/src/*
+gsutil -m setmeta -r -h "Cache-Control:public, max-age=60" gs://static.gamefroot.com/blockly/2.2.0/*
